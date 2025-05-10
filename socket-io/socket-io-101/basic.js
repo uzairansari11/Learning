8d02445a8499a1245222e672c845d0f260240f2e
Uzair Ansari <uzairansari@Uzairs-MacBook-Air.local>
2025-05-10 18:49:39 +0530

    normal chat using socket implemented

---
+++ socket-io/socket-io-101/basic.js
@@ -0,0 +1,21 @@
+const express = require("express")
+
+const app = express()
+
+const socketIo = require("socket.io")
+
+app.use(express.static(__dirname + "/public"))
+console.log("dir",__dirname+"/public")
+const expressServer = app.listen(8000)
+
+const io = socketIo(expressServer)
+
+io.on("connect", (socket) => {
+      console.log("socket id", socket.id)
+      
+      socket.emit("messageFromServer", { data: "Hello form server your socket id is" + socket.id })
+      
+      socket.on("messageFromClient", (data) => {
+            console.log("data",data)
+      })
+})
